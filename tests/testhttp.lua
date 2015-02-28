ece81a1dd7a972d373105907f375773f959122a0
Thijs Schreijer <thijs@thijsschreijer.nl>
2015-02-28 12:07:43 +0100

    added test for async http command

--- tests/testhttp.lua
+++ tests/testhttp.lua
@@ -1,4 +1,8 @@
 local copas = require("copas")
 local http = require("copas.http")
 
-copas.add
+copas.addthread(function()
+  print(copas.http.request("http://www.google.com"))
+end)
+
+copas.loop()
