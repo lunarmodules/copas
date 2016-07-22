f3e6123d0dcb65a95c62a2880ec45585426ab129
SpiralP <spiral.palmer@gmail.com>
2016-07-21 18:06:39 -0700

    fix

--- lua/includes/modules/copas.lua
+++ lua/includes/modules/copas.lua
@@ -502,12 +502,12 @@ local _skt_mt_tcp = {
 
                    accept = function(self, ...) return self.socket:accept(...) end,
 
-                   setoption = function(self, ...) return self.setoption:accept(...) end,
+                   setoption = function(self, ...) return self.socket:accept(...) end,
 
                    -- TODO: is this DNS related? hence blocking?
                    getpeername = function(self, ...) return self.socket:getpeername(...) end,
 
-                   shutdown = function(self, ...) return self.shutdown:accept(...) end,
+                   shutdown = function(self, ...) return self.socket:accept(...) end,
 
                    dohandshake = function(self, sslt)
                      self.ssl_params = sslt or self.ssl_params
