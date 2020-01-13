e703b8f0700f20af981a1666a0aa931011462ec5
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-01-13 11:36:42 +0100

    fix(test) fixes the test for redirecting http->http
    fixes #75

--- tests/httpredirect.lua
+++ tests/httpredirect.lua
@@ -5,6 +5,7 @@ local http = require("copas.http")
 local ltn12 = require("ltn12")
 local dump_all_headers = false
 local redirect
+local socket = require "socket"
 
 
 local function doreq(url)
@@ -43,7 +44,25 @@ copas.addthread(function()
     assert(headers.location == "https://github.com/brunoos/luasec")
     print("http  -> https redirect OK!")
     copas.addthread(function()
-      local result, code, headers, status = doreq("http://goo.gl/tBfqNu")  -- http --> http redirect
+      --local result, code, headers, status = doreq("http://goo.gl/tBfqNu")  -- http --> http redirect
+      -- the above no longer works for testing, since Google auto-inserts a
+      -- initial redirect to the same url, over https, hence the final
+      -- redirect is a downgrade which then errors out
+      -- so we set up a local http-server to deal with this
+      local server = assert(socket.bind("127.0.0.1", 9876))
+      local crlf = string.char(13)..string.char(10)
+      copas.addserver(server, function(skt)
+          skt = copas.wrap(skt)
+          local request = assert(skt:receive())
+          local response =
+              "HTTP/1.1 302 Found" .. crlf ..
+              "Location: http://www.thijsschreijer.nl/blog/" .. crlf .. crlf
+          assert(skt:send(response))
+          skt:close()
+      end)
+      -- execute test request
+      local result, code, headers, status = doreq("http://localhost:9876/")  -- http --> http redirect
+      copas.removeserver(server)  -- immediately close server again
       assert(tonumber(code)==200)
       assert(headers.location == "http://www.thijsschreijer.nl/blog/")
       print("http  -> http  redirect OK!")
