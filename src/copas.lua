c4f531d5306b4b3c8adec4a618706f67dde16ea1
Thijs Schreijer <thijs@thijsschreijer.nl>
2026-08-15 11:41:00 +0200

    release 4.12.0

--- src/copas.lua
+++ src/copas.lua
@@ -136,7 +136,7 @@ end
 -- Meta information is public even if beginning with an "_"
 copas._COPYRIGHT   = "Copyright (C) 2005-2013 Kepler Project, 2015-2026 Thijs Schreijer"
 copas._DESCRIPTION = "Coroutine Oriented Portable Asynchronous Services"
-copas._VERSION     = "Copas 4.11.0"
+copas._VERSION     = "Copas 4.12.0"
 
 -- Close the socket associated with the current connection after the handler finishes
 copas.autoclose = true
